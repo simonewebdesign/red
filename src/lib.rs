44b943e252f233b4cb20612a29f7895781c51eaa
Simone Vittori <svitto@svit.to>
2020-05-17 21:18:52 +0100

    Move functions into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn init() -> std::io::Result<()> {
+    let mut store = File::create("redstore")?;
+    store.write_all(b"11111 foo@example.com\n22222 bar@example.com\n33333 baz@example.com\n")?;
+    Ok(())
+}
+
+pub fn read_store() {
+    let contents = fs::read_to_string("redstore")
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
 
+mod lib;
+
 fn main() {
-    if let Err(_) = init() {
+    if let Err(_) = lib::init() {
         println!("red: error: failed to initialize store");
         process::exit(1);
     }
-    read_store();
-}
-
-fn init() -> std::io::Result<()> {
-    let mut store = File::create("redstore")?;
-    store.write_all(b"11111 foo@example.com\n22222 bar@example.com\n33333 baz@example.com\n")?;
-    Ok(())
-}
-
-fn read_store() {
-    let contents = fs::read_to_string("redstore")
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    lib::read_store();
 }

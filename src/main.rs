77bd8aced6cb77646404ab40047162235b05504d
Simone Vittori <svitto@svit.to>
2020-05-17 18:53:17 +0100

    Read sample data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -7,10 +8,18 @@ fn main() {
         println!("red: error: failed to initialize store");
         process::exit(1);
     }
+    read_store();
 }
 
 fn init() -> std::io::Result<()> {
     let mut store = File::create("redstore")?;
-    store.write_all(b"Hello, world!")?;
+    store.write_all(b"11111 foo@example.com\n22222 bar@example.com\n33333 baz@example.com\n")?;
     Ok(())
+}
+
+fn read_store() {
+    let contents = fs::read_to_string("redstore")
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }

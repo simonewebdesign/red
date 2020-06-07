477f35f70edf7d4176e8db588455c502ca2a7d76
Simone Vittori <svitto@svit.to>
2020-06-07 14:01:12 +0100

    Add stub command implementations in server

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,19 @@
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::str;
+mod lib;
+use lib::State;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     listener.set_nonblocking(true).expect("Cannot set non-blocking");
 
+    let mut state = State::new();
+
     for stream in listener.incoming() {
         match stream {
             Ok(s) => {
-                handle_conn(s);
+                handle_conn(s, &mut state);
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 continue;
@@ -19,12 +23,12 @@ fn main() {
     }
 }
 
-fn handle_conn(mut stream: TcpStream) {
+fn handle_conn(mut stream: TcpStream, state: &mut State) {
     let mut buf = vec![];
     loop {
         match stream.read_to_end(&mut buf) {
             Ok(_) => {
-                handle_bytes(&buf);
+                handle_bytes(&buf, state);
                 break;
             },
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
@@ -35,12 +39,38 @@ fn handle_conn(mut stream: TcpStream) {
     };
 }
 
-fn handle_bytes(mut buffer: &[u8]) {
+fn handle_bytes(mut buffer: &[u8], state: &mut State) {
     println!("result: {:?}", buffer);
     match buffer {
+        // GET key
+        [103, 101, 116, ..] => {
+
+        }
+
+        // SADD member
+        [115, 97, 100, 100, ..] => {
+
+        }
+
+        // SMEMBERS
+        [115, 109, 101, 109, 98, 101, 114, 115, ..] => {
+
+        }
+
+        // SREM member
+        [115, 114, 101, 109, ..] => {
+
+        }
+
         // SET key value
         [115, 101, 116, ..]  => {
-            println!("it's a set operation");
+            state.set("somekey".to_string(), "someValue".to_string());
+            println!("it's a set operation. A sample key has been added to the state.");
+        }
+
+        // DEBUG
+        [100, 101, 98, 117, 103, ..] => {
+            println!("{:#?}", state);
         }
 
         _ => {
